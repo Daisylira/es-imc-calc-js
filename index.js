66811e2e88615c671b85a51b69ba582722990ac9
Daisy <daisy.lira@hotmail.com>
2021-04-12 19:24:39 -0300

    feat: removed comments

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function createRequest() {
       request = new ActiveXObject("Msxml2.XMLHTTP");
     } catch (otherMS) {
       try {
-      request = new ActiveXObject("Microsoft.XMLHTTP");
+        request = new ActiveXObject("Microsoft.XMLHTTP");
       } catch (failed) {
         console.log('no way to create XMLHttpRequest object')
       }
@@ -21,7 +21,7 @@ function calculateImcAPI(person) {
   var request = createRequest();
   if (!request) return null;
 
-  request.onreadystatechange = function() {
+  request.onreadystatechange = function () {
     if (this.readyState === 4) {
       if (this.status === 200) {
         renderImc(JSON.parse(this.responseText));
@@ -42,10 +42,10 @@ function renderImc(person) {
 }
 
 function Person(height, weight) {
-  if (typeof(height) !== 'number' || isNaN(height))
+  if (typeof (height) !== 'number' || isNaN(height))
     throw Error('height is not a number!');
 
-  if (typeof(weight) !== 'number' || isNaN(weight))
+  if (typeof (weight) !== 'number' || isNaN(weight))
     throw Error('weight is not a number!');
 
   this.height = height;
@@ -71,66 +71,55 @@ function buildCalculateImc() {
   var heightEl = document.getElementById('altura');
   var weightEl = document.getElementById('peso');
 
-  return function(evt) {
+  return function (evt) {
     calculateImc(new Dietician(parseFloat(heightEl.value), parseFloat(weightEl.value)));
   }
 }
 
 
-window.onload = function() {
+window.onload = function () {
   var btn = document.querySelector('.data .form button');
   btn.addEventListener('click', buildCalculateImc());
 }
 
 // Atividade 12/07/2021
-// #Daisy L. O. Salgado – RA:1905248 – #daisy.salgado@aluno.faculdadeimpacta.com.br 
-//Guilherme Costa Silva – RA:2100102 – #guilherme.csilva@aluno.faculdadeimpacta.com.br 
-//Kleber Eiji Yokomizo Oshita – RA:1905475 – #kleber.oshita@aluno.faculdadeimpacta.com.br 
-//Ramon D. Freitas – RA:1905270 – #ramon.freitas@aluno.faculdadeimpacta.com.br
-//Thiago Pereira Silveira – RA:1905339 – #	thiago.psilveira@aluno.faculdadeimpacta.com.br
 
 // api url
-const api_url = 
+const api_url =
   "http://localhost:8080/imc/table";
-  
+
 // Defining async function
 async function getapi(url) {
-    
-    // Storing response
-    const response = await fetch(url);
-    
-    // Storing data in form of JSON
-    var data = await response.json();
-    console.log(data);
-    if (response) {
-        hideloader();
-    }
-    show(data);
+
+  // Storing response
+  const response = await fetch(url);
+
+  // Storing data in form of JSON
+  var data = await response.json();
+  console.log(data);
+  if (response) {
+    hideloader();
+  }
+  show(data);
 }
 // Calling that async function
 getapi(api_url);
-  
+
 // Function to hide the loader
 function hideloader() {
-    document.getElementById('loading').style.display = 'none';
+  document.getElementById('loading').style.display = 'none';
 }
 // Function to define innerHTML for HTML table
 function show(data) {
-    let tab = 
-        `<tr>
-          <th>Tipos</th>
-         </tr>`;
-    
-    // Loop to access all rows 
-    for (let r of data.list) {
-        tab += `<tr> 
-    <td>${0} </td>
-    <td>${99}</td>
-    <td>${18.5}</td> 
-    <td>${24.9}</td>          
-</tr>`;
-    }
-    // Setting innerHTML as tab variable
-    document.getElementById("tabelaimc").innerHTML = tab;
+  let tab =
+    `<tr>
+        <th>Tipos</th>
+      </tr>
+      <tr>
+        <td>${data} </td>
+      </tr>`;
+
+  // Setting innerHTML as tab variable
+  document.getElementById("tabelaimc").innerHTML = tab;
 }
 
